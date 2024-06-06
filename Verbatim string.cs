9c537ef708257102d60ce39ebbc9b9e2548deade
SP4ZE <102462290+SP4ZE-DEV@users.noreply.github.com>
2024-06-06 01:29:52 -0800

    Add files via upload

---
+++ Verbatim string.cs
@@ -0,0 +1,26 @@
+﻿// welcome messages
+
+Console.Write(@"
+
+
+
+
+\u306b\
+WELCOME TO THE EXILE
+
+    enjoy your stay
+      - balls
+                ,;,
+                *|*
+   ,;,         _*|*_
+   *|*        /     \   
+   /\      /|/ \   / \  /\      ,;,
+  /  \/\  | /    /    |/  |\    *|*  __/\
+_/   /  \/ /|   /\     \    \ ___|__/ /  \_
+
+
+
+
+
+"); 
+Console.WriteLine("\u6c42");
