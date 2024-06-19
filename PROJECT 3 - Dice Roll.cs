f0b0ef5f5ad5a49beee3b875874d55c93619d341
SP4ZE <102462290+SP4ZE-DEV@users.noreply.github.com>
2024-06-18 22:43:20 -0800

    Create PROJECT 3 - Dice Roll.cs

---
+++ PROJECT 3 - Dice Roll.cs
@@ -0,0 +1,50 @@
+// GAMBLING SIMULATOR
+
+// random class
+Random rand = new(); // creates an instance of a new random class
+
+// assigning random values
+int throw1 = 2;
+int throw2 = 3;
+int throw3 = 3;
+
+int total = throw1 + throw2 + throw3;
+
+// output simple throw info
+Console.WriteLine($"Your throws are {throw1} + {throw2} + {throw3} = {total}");
+
+
+
+// tripples bonus check
+if (throw1 == throw2 && throw2 == throw3 && throw3 == throw1)
+{
+    total += 3;
+    Console.WriteLine("\n\tYou rolled tripples! + 3 bonus");
+}
+
+// BONUS
+else if (throw1 == throw2 || throw2 == throw3 || throw3 == throw1)
+{
+    total += 2;
+    // output bonus info
+    Console.WriteLine("\n\tYou rolled doubles! +2 to total");
+}
+
+
+// WIN assesment
+if (total == 21) 
+{
+    Console.WriteLine("\n\t\tYou win 1000$ !");
+}
+else if (total >= 16)
+{
+    Console.WriteLine("\n\t\tYou win 333$ !");
+}
+else if (total >= 12)
+{
+    Console.WriteLine("\n\t\tYou win 111$ !");
+}
+else
+{
+    Console.WriteLine("\n\t\tYou lost... ");
+}
